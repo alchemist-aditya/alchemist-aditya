7257400b7009240de52d3157dd15884f3ca04e16
alchemist-aditya <adityapandya2185@gmail.com>
2025-07-30 09:18:06 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useCallback } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
-import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 import { motion } from "framer-motion";
 import "./styles.css";
 import VideoPortfolio from "./videoportfolio";
